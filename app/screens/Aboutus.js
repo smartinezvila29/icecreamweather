1868a0936512dbf874e7d020474ce58049f6b4a4
cristian <cristianariel.sanhueza@gmail.com>
2021-11-22 15:46:25 -0300

    Modificaciones sobre About US

--- app/screens/Aboutus.js
+++ app/screens/Aboutus.js
@@ -1,10 +1,87 @@
 import React from "react";
-import { View, Text} from "react-native";
+import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
+import { color } from "react-native-reanimated";
 
 export default function Aboutus() {
-    return (
+  return (
+    <ScrollView stayle={{ flex: 1, backgroundColor: "#6ED4C8" }}>
+      <View style={{ flexDirection: "row" }}>
+        <Image
+          style={styles.banner}
+          source={require("../assets/img/climapp_banner.png")}
+        />
+      </View>
+      <View style={styles.contenedor}>
+        <Text style={styles.titulo}> About Us</Text>
+        <ScrollView horizontal>
+          <View>
+            <Image
+              style={styles.presentar}
+              source={require("../assets/img/santiago_martinez.png")}
+            />
+          </View>
+          <View>
+            <Image
+              style={styles.presentar}
+              source={require("../assets/img/iara_olivera.png")}
+            />
+          </View>
+          <View>
+            <Image
+              style={styles.presentar}
+              source={require("../assets/img/gaston_figueredo.png")}
+            />
+          </View>
+          <View>
+            <Image
+              style={styles.presentar}
+              source={require("../assets/img/cristian_sanhueza.png")}
+            />
+          </View>
+        </ScrollView>
+      </View>
+      <View style={styles.contenedor}>
+        <Text style={styles.titulo}> About Application</Text>
         <View>
-            <Text>Who we ´re?</Text>
+          <Image
+            style={styles.foot}
+            source={require("../assets/img/gpl.png")}
+          />
         </View>
-        );
+        <View>
+          <Image
+            style={styles.foot}
+            source={require("../assets/img/fin.png")}
+          />
+        </View>
+      </View>
+    </ScrollView>
+  );
 }
+
+const styles = StyleSheet.create({
+  banner: {
+    height: 200,
+    flex: 1,
+  },
+  titulo: {
+    fontWeight: "bold",
+    fontSize: 24,
+    marginVertical: 10,
+    textAlign: "center",
+    color: "#002973"
+  },
+  contenedor: {
+    marginHorizontal: 10,
+  },
+  presentar: {
+    width: 330,
+    height: 250,
+    marginRight: 10,
+  },
+  foot:{
+      width:'100%',
+      height:250,
+      marginVertical:5
+  }
+});
